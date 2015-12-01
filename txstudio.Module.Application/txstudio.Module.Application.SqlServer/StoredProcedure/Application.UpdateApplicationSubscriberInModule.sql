76a258ce981aa47c74a11deb4d5f1a230610574f
Shi Hao Wang <st890409@hotmail.com>
2015-12-01 19:37:45 +0800

    更新指定應用程式訂閱的可使用模組資料

---
+++ txstudio.Module.Application/txstudio.Module.Application.SqlServer/StoredProcedure/Application.UpdateApplicationSubscriberInModule.sql
@@ -0,0 +1,34 @@
+﻿/*
+	更新指定應用程式訂閱可使用的模組資料
+*/
+CREATE PROCEDURE [Application].[UpdateApplicationSubscriberInModule]
+	@SubscriberNo	SMALLINT,
+	@ModuleNo		[Application].[ApplicationModule] READONLY,
+	
+	@Result			BIT
+AS
+
+	SET @Result = 0
+
+	MERGE [Application].[ApplicationSubscriberInModule] as target
+	USING (
+		SELECT [No] [ModuleNo]
+			,@SubscriberNo [SubscriberNo]
+		FROM @ModuleNo
+	) as source ([ModuleNo],[SubscriberNo]) ON (
+		target.[ModuleNo] = source.[ModuleNo]
+			AND target.[SubscriberNo] = source.[SubscriberNo]
+	)
+	WHEN NOT MATCHED BY source THEN
+		DELETE
+	WHEN NOT MATCHED BY target THEN
+		INSERT (
+			[ModuleNo]
+			,[SubscriberNo]
+		) VALUES (
+			source.[ModuleNo]
+			,source.[SubscriberNo]
+		);
+
+	SET @Result = 1
+GO
