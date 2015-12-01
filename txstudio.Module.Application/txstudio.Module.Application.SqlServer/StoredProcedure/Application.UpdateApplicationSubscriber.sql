05420c8ca511c3386360fac068c94eda6cd115c6
Shi Hao Wang <st890409@hotmail.com>
2015-12-01 19:24:07 +0800

    更新指定應用程式訂閱資料

---
+++ txstudio.Module.Application/txstudio.Module.Application.SqlServer/StoredProcedure/Application.UpdateApplicationSubscriber.sql
@@ -0,0 +1,23 @@
+﻿/*
+	更新指定應用程式訂閱資料
+*/
+CREATE PROCEDURE [Application].[UpdateApplicationSubscriber]
+	@Name				NVARCHAR(50),
+	@Description		NVARCHAR(250),
+	@IsEnabled			BIT,
+	@memberLastChange	NVARCHAR(50),
+	@No					SMALLINT,
+
+	@Result				BIT OUT
+AS
+
+	UPDATE [Application].[ApplicationSubscriber]
+	   SET [Name] = @Name
+		  ,[Description] = @Description
+		  ,[IsEnabled] = @IsEnabled
+		  ,[whenLastChange] = SYSDATETIMEOFFSET()
+		  ,[memberLastChange] = @memberLastChange
+	 WHERE [No] = @No
+
+GO
+
