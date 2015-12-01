440c2a65f04ec7b1af22292d7dd3293c2d1911a7
Shi Hao Wang <st890409@hotmail.com>
2015-12-01 19:17:34 +0800

    新增應用程式訂閱資料

---
+++ txstudio.Module.Application/txstudio.Module.Application.SqlServer/StoredProcedure/Application.AddApplicationSubscriber.sql
@@ -0,0 +1,32 @@
+﻿/*
+	新增一筆應用程式訂閱資料
+*/
+CREATE PROCEDURE [Application].[AddApplicationSubscriber]
+	@Schema			VARCHAR(25),
+	@Name			NVARCHAR(50),
+	@Description	NVARCHAR(250),
+	@IsEnabled		BIT,
+	@memberCreate	NVARCHAR(50),
+
+	@No				SMALLINT OUT
+AS
+
+	INSERT INTO [Application].[ApplicationSubscriber](
+		[Schema]
+		,[Name]
+		,[Description]
+		,[IsEnabled]
+		,[memberCreate]
+	) VALUES (
+		@Schema
+		,@Name
+		,@Description
+		,@IsEnabled
+		,@memberCreate
+	)
+	
+	SET @No = (
+		SELECT IDENT_CURRENT('Application.ApplicationSubscriber')
+	)
+
+GO
